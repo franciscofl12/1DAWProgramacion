96379fc9a2c289d9f5e346786696401be7525b20
franciscofl12 <xiskoyruben@gmail.com>
2020-10-28 08:56:46 +0100

    Up ejercicio 6

--- src/cap3Bloque3/Ejercicio6.java
+++ src/cap3Bloque3/Ejercicio6.java
@@ -27,17 +27,9 @@ public class Ejercicio6{
 			if (num == 0) {
 				salir = true;
 			}
-			System.out.println(" 0 x " + num + " = " + 0*num
-					+"\n 1 x " + num + " = " + 1*num
-					+"\n 2 x " + num + " = " + 2*num
-					+"\n 3 x " + num + " = " + 3*num
-					+"\n 4 x " + num + " = " + 4*num
-					+"\n 5 x " + num + " = " + 5*num
-					+"\n 6 x " + num + " = " + 6*num
-					+"\n 7 x " + num + " = " + 7*num
-					+"\n 8 x " + num + " = " + 8*num
-					+"\n 9 x " + num + " = " + 9*num
-					+"\n 10 x " + num + " = " + 10*num);
+			for (int i = 0; i < 10; i++) {
+			System.out.println(i + " x " + num + " = " + i*num);
+			}
     	}
 
     }
