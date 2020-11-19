fb3407c384ed9c83b235f469013d333a12a8e26d
franciscofl12 <xiskoyruben@gmail.com>
2020-11-19 09:35:29 +0100

    Update Ejercicio4.java

--- src/cap5/bloque6/Ejercicio4.java
+++ src/cap5/bloque6/Ejercicio4.java
@@ -6,24 +6,27 @@ public class Ejercicio4 {
 		matrices();
 	}
 	
+	@SuppressWarnings("unused")
 	public static void matrices() {
-		int[][] matriz = new int[3][3];
-		Ejercicio1y2.rellenarMatriz(matriz, 0, 1);
+		int[][] matriz = new int[][] { {1, 0, 0},
+									   {0, 1, 0},
+									   {0, 0, 1} };
+		//Ejercicio1y2.rellenarMatriz(matriz, 0, 1);
 		Utils.Utils.imprimirMatriz(matriz);
 		boolean ceros = true;
 		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz.length; j++) {
 				if (matriz[i][j] != 0 && (i != j)) {
 					ceros = false;
+					break;
 				}
 				else {
-					if (matriz[i][j] != 1 && (i == j)) {
-						ceros = false;
-				}
+					ceros = true;
 			}
 		}
+		break;
 	}
-	if (ceros=false) System.out.println("Matriz no diagonal");
-	if (ceros=true) System.out.println("Matriz diagonal");
+	if (ceros==false) System.out.println("Matriz no diagonal");
+	if (ceros==true) System.out.println("Matriz diagonal");
 }
 }
