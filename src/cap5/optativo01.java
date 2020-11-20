34a804bb600f8a1d74280bfe6bca30e3ab88a314
franciscofl12 <xiskoyruben@gmail.com>
2020-11-20 14:08:23 +0100

    Create optativo01.java

---
+++ src/cap5/optativo01.java
@@ -0,0 +1,16 @@
+package cap5;
+
+public class optativo01 {
+
+	public static void main(String[] args) {
+
+	}
+
+	/****
+	 * Realiza un programa que genere una apuesta aleatoria de la lotería
+	 * primitiva. Consiste en generar seis números y otro complementario entre 1 y
+	 * 49 que no se repitan. Ejemplo: Combinación: 2 14 20 37 39 41 complementario:
+	 * 13. Utiliza un array, en el ejemplo expuesto sería un array de 7 posiciones
+	 * correspondiendo la séptima posición al "complementario" de la loteria.
+	 */
+}
