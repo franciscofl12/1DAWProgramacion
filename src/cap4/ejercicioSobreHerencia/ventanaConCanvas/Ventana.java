7de8645c6330f01789135c14a81cd71741cc273f
Francisco Flores Lucena <54539494+franciscofl12@users.noreply.github.com>
2021-01-15 15:26:44 +0100

    ADD Ejercicio 4 Herencia y clases abstractas

---
+++ src/cap4/ejercicioSobreHerencia/ventanaConCanvas/Ventana.java
@@ -0,0 +1,10 @@
+package cap4.ejercicioSobreHerencia.ventanaConCanvas;
+
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame{
+	
+	public Ventana() {
+		this.setSize(500,500); //Pongo el tamano de la ventana
+	}
+}
